8e95618684d68ac4c656273718326802c8e6f3c1
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-05-02 19:43:45 -0300

    Refactor: Refatoração de código. O código foi refatorado sem alteração do funcionamento.

--- Text-Adventure/escapeFromSP.py
+++ Text-Adventure/escapeFromSP.py
@@ -1,14 +1,23 @@
 # Este jogo é uma aventura baseada em texto, que consiste em escapar de São Paulo.
-print("Escape de São Paulo!")
-print("Ações validas: Norte [n], Sul [s], Este [e], Oeste [o].")
-action_input = input("Digite uma direção: ")
-if action_input == "n":
-    print("Indo para o Norte!")
-elif action_input == "s":
-    print("Indo para o Sul!")
-elif action_input == "e":
-    print("Indo para o Este!")
-elif action_input == "o":
-    print("Indo para o Oeste!")
-else:
-    print("Direção invalida! Digite uma direção valida. ")
+
+def get_player_command():
+    return input("Digite uma direção: ")
+
+
+def play():
+    print("Escape de São Paulo!")
+    print("Ações validas: Norte [n], Sul [s], Este [e], Oeste [o].")
+    action_input = get_player_command()
+    if action_input in ["n", "N"]:
+        print("Indo para o Norte!")
+    elif action_input in ['s', 'S']:
+        print("Indo para o Sul!")
+    elif action_input in ['e', 'E']:
+        print("Indo para o Este!")
+    elif action_input in ['o', 'O']:
+        print("Indo para o Oeste!")
+    else:
+        print("Direção invalida! Digite uma direção valida. ")
+
+
+play()
