c7459a9ede17c60012efd87e2586395dc93e6889
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-05-04 23:17:33 -0300

    Feat: Ampliando armas. Foram apliadas as opções de armas.

--- Text-Adventure/escapeFromSP.py
+++ Text-Adventure/escapeFromSP.py
@@ -25,11 +25,23 @@ class Espingarda:
     def __str__(self):
         return self.nome
 
-class Espingarda:
+class Carabina:
     def __int__(self):
         self.nome = "Escopeta 12mm"
-        self.descricao = "Uma espingarda calibre 12mm, normalmente usada por policiais militares."
+        self.descricao = "Carabina é uma arma de fogo mais curta que o fuzil, tendo entre 1,0 e 1,2 \n" \
+                         "metro de comprimento muito usada em caça e tiro desportivo podendo ser de \n" \
+                         "diversos calibres."
         self.dano = 5
+        self.especial = "Pode ser acoplada uma lente de precisão a Carabina de até x8."
+    def __str__(self):
+        return self.nome
+
+class Frigideira:
+    def __int__(self):
+        self.nome = "Frigideira"
+        self.descricao = "Um utensílio usado na cozinha para fritar alimentos."
+        self.dano = 7
+        self.especial = "Faz um barulho engraçado quando bate na cabeça do inimigo."
     def __str__(self):
         return self.nome
 
