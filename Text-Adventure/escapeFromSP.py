d50b7ab57e73029d8a5da8f1713144a589ca0c79
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-05-06 11:37:55 -0300

    Feat: Adicionando armas e objetos. Foram adicionadas mais classes ao jogo.

--- Text-Adventure/escapeFromSP.py
+++ Text-Adventure/escapeFromSP.py
@@ -1,31 +1,33 @@
 # Este jogo é uma aventura baseada em texto, que consiste em escapar de São Paulo.
 
+# Armas
+class Arma:
+    def __str__(self):
+        return self.nome
 
-class Faca:
+
+class Faca(Arma):
     def __int__(self):
         self.nome = "Faca"
         self.descricao = "Uma faca cerrilhada meio amolada simples."
         self.dano = 10
-    def __str__(self):
-        return self.nome
 
-class Pedra:
+
+class Pedra(Arma):
     def __int__(self):
         self.nome = "Pedra"
         self.descricao = "Uma pedra um pouco maior que uma bola de tenis, ideal para arremeçar."
         self.dano = 5
-    def __str__(self):
-        return self.nome
 
-class Espingarda:
+
+class Espingarda(Arma):
     def __int__(self):
         self.nome = "Escopeta 12mm"
         self.descricao = "Uma espingarda calibre 12mm, normalmente usada por policiais militares."
         self.dano = 70
-    def __str__(self):
-        return self.nome
 
-class Carabina:
+
+class Carabina(Arma):
     def __int__(self):
         self.nome = "Escopeta 12mm"
         self.descricao = "Carabina é uma arma de fogo mais curta que o fuzil, tendo entre 1,0 e 1,2 \n" \
@@ -33,17 +35,15 @@ class Carabina:
                          "diversos calibres."
         self.dano = 5
         self.especial = "Pode ser acoplada uma lente de precisão a Carabina de até x8."
-    def __str__(self):
-        return self.nome
 
-class Frigideira:
+
+class Frigideira(Arma):
     def __int__(self):
         self.nome = "Frigideira"
         self.descricao = "Um utensílio usado na cozinha para fritar alimentos."
         self.dano = 7
         self.especial = "Faz um barulho engraçado quando bate na cabeça do inimigo."
-    def __str__(self):
-        return self.nome
+
 
 def get_player_command():
     return input("Digite uma ação: ")
@@ -56,11 +56,12 @@ def play():
     Ir para o Sul   [s ou S]
     Ir para o Este  [e ou E]
     Ir para o Oeste [o ou O]
-    Ver Inventário  [i ou I]
-    Procurar na area [b ou B]\n''')
+    Ver Mochila  [i ou I]
+    Procurar na area [p ou P]\n''')
 
     action_input = get_player_command()
-    inventory = [
+    mochila = [
+        Pedra(),
         'Mochila',
         'Revolver 38mm',
         'Corda 10m',
@@ -75,6 +76,7 @@ def play():
         'lente de óculos',
         'Kit primeiros-socorros'
     ]
+
 # MARK: GAME LOOP
     if action_input in ["n", "N"]:
         print("Viajar para o Norte!")
@@ -85,16 +87,15 @@ def play():
     elif action_input in ['o', 'O']:
         print("Viajar para o Oeste!")
     elif action_input in ['i', 'I']:
-        print("Inventário:\n")
-        print(inventory)
-    elif action_input in ['b', 'B']:
+        print("Mochila:\n")
+        print(mochila)
+    elif action_input in ['p', 'P']:
         print("Basculhando a área...\n\n")
         print("Itens encontrados:\n")
         print(items)
         for i in items:
-            inventory.append(i)
+            mochila.append(i)
     else:
         print("Ação invalida! Digite uma direção valida. ")
-
 
 play()
